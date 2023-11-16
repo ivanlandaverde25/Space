a9806d4bdbe3a44817798e7c2677fcdeed7b8e5d
Iván Landaverde <2502882019@mail.utec.edu.sv>
2023-11-16 09:30:02 -0600

    Ddestination avanzado

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 window.addEventListener('DOMContentLoaded', () => {
-    console.log("HOLIS");
+    let container = document.querySelector('.container');
+    let botonHome = document.getElementById('boton-home');
+    let botonDestination = document.getElementById('boton-destination');
+
+    if ( botonHome ){
+        botonHome.addEventListener('click', () => {
+            console.log("Boton Home clicked");
+            // container.style.background = 'url("../assets/Desktop/background-home-desktop.jpg") no-repeat center/cover';
+        });
+    }
+    
+    if ( botonDestination ){
+        botonDestination.addEventListener('click', () => {
+            console.log("Boton Destination clicked");
+            // container.style.background = 'url("../assets/Desktop/background-destination-desktop.jpg") no-repeat center/cover';
+        });
+    }
+
 });
